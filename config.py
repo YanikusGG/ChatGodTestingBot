9f1ca2841e4604c0b0549ba61580417acff095c0
Yan Anikiev <yanikusgg@yandex.ru>
2022-12-19 02:51:26 +0300

    add parsing method

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 ﻿# bot http://t.me/ChatGodTestingBot
 TOKEN = '<bot_api_token>'
+
+DEFAULT_POSTS_LIMIT = 10
+MAX_REQUESTS_PER_CHANNEL = 30
--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from bs4 import BeautifulSoup
 from datetime import datetime
+import re
+import requests
 
-from config import TOKEN
+from config import TOKEN, DEFAULT_POSTS_LIMIT, MAX_REQUESTS_PER_CHANNEL
 
 
 logging.basicConfig(level=logging.INFO)
@@ -81,6 +84,90 @@ async def send_calculation(message: types.Message, state: FSMContext):
         result = 'Некорректное выражение!'
     await message.answer("Результат: " + str(result),
                          reply_markup=keyboard_start)
+
+@dp.message_handler(Text(["Парсинг канала"]))
+async def start_parsing(message: types.Message):
+    await BotStates.parsing.set()
+    await message.answer("Напиши, какой публичный канал нужно распарсить, в формате `@channel_name`",
+                         reply_markup=types.ReplyKeyboardRemove())
+
+async def get_last_image_id(channel):
+    try:
+        channel_url = f'https://t.me/s/{channel}'
+        req = requests.get(channel_url)
+        soup = BeautifulSoup(req.content.decode("utf-8"), 'html.parser')
+        images = soup.find_all('a', {'class': 'tgme_widget_message_photo_wrap'})
+        hrefs = [image.get('href')[len(channel_url)-1:] for image in images]
+        return max(int(href[:-7] if href[-7:] == '?single' else href) for href in hrefs)
+    except Exception as e:
+        print(e)
+        return 0
+
+async def parse_image(channel, image_id):
+    try:
+        req = requests.get(f'https://t.me/s/{channel}/{image_id}')
+        soup = BeautifulSoup(req.content.decode("utf-8"), 'html.parser')
+        image = soup.find('a', {'class': 'tgme_widget_message_photo_wrap', 'href': f'https://t.me/{channel}/{image_id}'}) or \
+                soup.find('a', {'class': 'tgme_widget_message_photo_wrap', 'href': f'https://t.me/{channel}/{image_id}?single'})
+        if image is None:
+            return None, None
+        url = re.match(r".*background-image:url\('(.*?)'\).*", image.get('style')).group(1)
+        if re.match(r".*/N.*", url):
+            return None, None
+        if image.get('href')[-7:] == '?single':
+            text = image.parent.parent.parent.parent.find('div', {'class': 'tgme_widget_message_text'})
+        else:
+            text = image.parent.find('div', {'class': 'tgme_widget_message_text'})
+        txt = None
+        if text:
+            txt = '\n'.join(text.stripped_strings)
+        return url, txt
+    except Exception as e:
+        print(e)
+        return None, None
+
+
+async def get_images_from_channel(channel):
+    try:
+        posts_limit = DEFAULT_POSTS_LIMIT
+
+        res_images = []
+        image_id = await get_last_image_id(channel)
+        request_count = 0
+
+        while image_id > 0 and request_count < MAX_REQUESTS_PER_CHANNEL and len(res_images) < posts_limit:
+            url, txt = await parse_image(channel, image_id)
+            post_url = f'https://t.me/{channel}/{image_id}'
+            image_id -= 1
+            request_count += 1
+            if url is None:
+                continue
+            if txt:
+                caption = txt + '\n' + post_url
+            else:
+                caption = post_url
+            res_images.append((url, caption))
+        return res_images
+    except Exception as e:
+        print(e)
+        return []
+
+@dp.message_handler(state=BotStates.parsing)
+async def send_parsed(message: types.Message, state: FSMContext):
+    await state.finish()
+    try:
+        channel = message.text[1:]
+        images = await get_images_from_channel(channel)
+        if images:
+            await message.answer(f"Последние картинки из канала @{channel}:")
+            media = [types.input_media.InputMediaPhoto(media=image[0], caption=image[1]) for image in images]
+            await message.answer_media_group(types.input_media.MediaGroup(media))
+            await message.answer(f"Фух, распарсил! Я молодец!!",
+                                 reply_markup=keyboard_start)
+    except Exception as e:
+        print(e)
+        await message.answer("Произошла ошибка(",
+                             reply_markup=keyboard_start)
 
 
 if __name__ == '__main__':
